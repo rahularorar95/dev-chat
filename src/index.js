8e4ef93083b77d84d0d25c9264fb98e9acf71f79
Rahul Arora <rahularorar95@gmail.com>
2019-03-30 20:25:10 +0530

    Added on auth change in index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import Register from './components/Auth/Register'
-import Login from './components/Auth/Login'
-import 'semantic-ui-css/semantic.min.css'
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./components/App"
+import { Router, Route, Switch } from "react-router-dom"
+import Register from "./components/Auth/Register"
+import Login from "./components/Auth/Login"
+import firebase from "./firebase"
+import history from './history'
+import "semantic-ui-css/semantic.min.css"
 
-const Root =()=>(
-    <BrowserRouter>
-        <Switch>
-            <Route path="/" exact component={App} />
-            <Route path="/login" exact component={Login} />
-            <Route path="/register" exact component={Register} />
-        </Switch>
-    </BrowserRouter>
-)
+class Root extends React.Component {
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                history.push("/")
+            }
+        })
+    }
+    render() {
+        return (
+            <Router history={history}>
+                <Switch>
+                    <Route path='/' exact component={App} />
+                    <Route path='/login' exact component={Login} />
+                    <Route path='/register' exact component={Register} />
+                </Switch>
+            </Router>
+        )
+    }
+}
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+ReactDOM.render(<Root />, document.getElementById("root"))
