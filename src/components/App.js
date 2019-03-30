f20c56b050cc2fa3afba9a563f271b7c64e86577
Rahul Arora <rahularorar95@gmail.com>
2019-03-30 22:55:47 +0530

    Side Panel version 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component } from "react"
+import { Grid } from "semantic-ui-react"
+import ColorPanel from "./ColorPanel/ColorPanel"
+import SidePanel from "./SidePanel/SidePanel"
+import MetaPanel from "./MetaPanel/MetaPanel"
+import Messages from "./Messages/Messages"
 import "./App.css"
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        heo
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Grid columns='equal' className='app' style={{ background: "#f6f6f6" }}>
+            
+                <ColorPanel />
+                <SidePanel />
+
+                <Grid.Column style={{ marginLeft: 320 }}>
+                    <Messages />
+                </Grid.Column>
+
+                <Grid.Column width={4}>
+                    <MetaPanel />
+                </Grid.Column>
+
+            </Grid>
+        )
+    }
 }
 
-export default App;
+export default App
