f2a50a737580e61fc32dfc75964450a9406794f9
Rahul Arora <rahularorar95@gmail.com>
2019-04-15 01:21:32 +0530

    code cleanup

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -76,7 +76,7 @@ class UserPanel extends Component {
     uploadCroppedImage = () => {
         const { storageRef, userRef, blob, metadata } = this.state
         storageRef
-            .child(`avatar/user-${userRef.uid}`)
+            .child(`avatar/users/${userRef.uid}`)
             .put(blob, metadata)
             .then(snap => {
                 snap.ref.getDownloadURL().then(downloadURL => {
